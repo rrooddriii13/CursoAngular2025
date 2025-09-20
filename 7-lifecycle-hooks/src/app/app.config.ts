2adc4748d2855d735edafb8f48f562024d0843c5
rodrigo <rodrigoencabo18@gmail.com>
2025-09-20 16:01:18 +0200

    seccion 16

---
+++ 7-lifecycle-hooks/src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { ApplicationConfig, provideZoneChangeDetection ,provideExperimentalZonelessChangeDetection} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    // provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideExperimentalZonelessChangeDetection(),
+  ],
+};
