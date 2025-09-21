5615af7020d0c9ec8ff07eff6571a1ebfd7ae598
rodrigo <rodrigoencabo18@gmail.com>
2025-09-21 16:46:48 +0200

    seccion msapas prto los marcadores ñe

--- 3-gifs-app/src/environments/enviroment.ts
+++ 3-gifs-app/src/environments/enviroment.ts
@@ -9,6 +9,7 @@ export const environment = {
   giphyUrl: 'https://api.giphy.com/v1',
 
   // URLS
+  MAPBOX_KEY:'pk.eyJ1Ijoiam9zZWFuZ2VsIiwiYSI6ImNrbmJmY2Z1ZTAyemYycXBndHFmeG1oZ3gifQ._-t0eL3pXqYk3n7d8dH4Xw'
 };
 
 // ng g env
